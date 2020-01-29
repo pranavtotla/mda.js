04d90a1d400e21056d3224db198d4a5ad161f386
Pranav Totla <thepranavtotla@gmail.com>
2020-01-29 13:41:21 +0530

    Added webpack config to build

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'mda.js',
+    library: 'mda'
+  },
+  optimization: {
+      minimize : false
+  }
+};
